60b39dbe0794b89afa74e7cd68253e8d585493b3
nniel-ape <nazandr72@gmail.com>
2026-05-12 20:21:11 +0300

    fix: update coreDirs paths after launcher moved to internal/

--- internal/launcher/launcher.go
+++ internal/launcher/launcher.go
@@ -106,9 +106,9 @@ func (l *Launcher) coreDirs() []string {
 		filepath.Join(l.ModuleRoot, "bus"),
 		filepath.Join(l.ModuleRoot, "settings"),
 		filepath.Join(l.ModuleRoot, "utils", "truncate"),
-		filepath.Join(l.ModuleRoot, "launcher"),
+		filepath.Join(l.ModuleRoot, "internal", "launcher"),
 		filepath.Join(l.ModuleRoot, "internal", "auth"),
-		filepath.Join(l.ModuleRoot, "cmd", "weave", "extmanage"),
+		filepath.Join(l.ModuleRoot, "internal", "extmanage"),
 	}
 }
 
